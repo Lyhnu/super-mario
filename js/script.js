523d147de1254f59a4864c53a45087cfef42c522
Lyhnu <contact@lyhnu.com>
2016-06-16 10:37:14 +0200

    Coins + count

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ $(function(){
 		if (posX == -2600 ) {
 			$(".end-screen").fadeIn(200, function() {
 			});
+			$(".count").addClass('endCount');
 		}
 	});
 
@@ -49,10 +50,16 @@ $(function(){
 	});
 
 	//detect flowers and coins positions
-	var tab_flower = new Array();
+	var tabFlower = new Array();
+	var tabCoin = new Array();
 	$(".flower").each(function(index){
-		tab_flower.push({"flower_object": $(this),
-			             "position_left": $(this).position().left});
+		tabFlower.push({"objectFlower": $(this),
+			             "posFlowerLeft": $(this).position().left});
+	});
+	$(".coin").each(function(index){
+		tabCoin.push({"objectCoin": $(this),
+			          "posCoinLeft": $(this).position().left,
+					  "posCoinTop": $(this).position().top});
 	});
 
 	//movement map + peach + stop map
@@ -65,6 +72,10 @@ $(function(){
 		left: "+=10px"
 	}
 
+	moveTop = {
+		top: "-=30px"
+	}
+
 	$(document).keydown(function(event){
 		var posX = $(".map").position().left;
 		if (event.which == 68 && posX > -2600) { //68 = D
@@ -72,31 +83,55 @@ $(function(){
 		} else if (event.which == 81 && posX < 0 || event.which == 65 && posX < 0) { //81 = Q, 65 = A
 			$(".map").animate(moveLeft, 0);
 		} else if (event.which == 90 || event.which == 87) { //90 = Z, 87 = W
-			$(".peach").animate({'top':pos.top - 100 + 'px'}, 250);
+			$(".peach").animate({'top':pos.top - 120 + 'px'}, 250);
 			setTimeout(function(){
 		    	$('.peach').animate({'top': pos.top + 'px'}, 50);
-		    }, 200);
+		    }, 400);
 		}
 
-		//catch flowers
+		//catch flowers & add score
 		var posMapX = $(".map").position().left;
 		var posPeachX = Math.abs(posMapX) + 165;
 		var nbFlowers = parseInt($(".flowers span").text());
-		var flower_to_remove = -1;
+		var removeFlower = -1;
 
-		$.each(tab_flower, function(index, value) {
-			var distX = Math.abs(value.position_left - posPeachX);
-			console.log( index + ": ", distX );
+		$.each(tabFlower, function(index, value) {
+			var distX = Math.abs(value.posFlowerLeft - posPeachX);
+			console.log(index + ": ", distX);
 			if (distX < 46) {
-				$(value.flower_object).animate({ opacity: 0, bottom: "80px" }, 600);
-				flower_to_remove = index;
+				$(value.objectFlower).animate({opacity: 0, bottom: "80px"}, 600);
+				removeFlower = index;
 				nbFlowers++
 				$(".flowers span").text(nbFlowers);
 			}
 		});
 
-		if (flower_to_remove !== -1) {
-			tab_flower.splice(flower_to_remove, 1);
+		if (removeFlower !== -1) {
+			tabFlower.splice(removeFlower, 1);
+		}
+
+		//catch coins & add score
+		var posMapY = $(".map").position().top;
+		var topPeach = $(".peach").position().top;
+		var posPeachY = topPeach;
+		var nbCoins = parseInt($(".coins span").text());
+		var removeCoin = -1;
+
+		$.each(tabCoin, function(index, value) {
+			var distY = Math.abs(value.posCoinTop - posPeachY);
+			var distX = Math.abs(value.posCoinLeft - posPeachX);
+			console.log(index + ": ", distX);
+			if (distX < 46 && distY < 50) {
+				$(value.objectCoin).animate({opacity:0}, { queue: false, duration: 600 });
+				$(value.objectCoin).animate(moveTop, { queue: false, duration: 600 });
+				removeCoin = index;
+				nbCoins++
+				$(".coins span").text(nbCoins);
+			}
+		});
+
+		if (removeCoin !== -1) {
+			tabCoin.splice(removeCoin, 1);
 		}
 	});
 });
